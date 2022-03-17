d5adfceca574f28576625728211bb2416c656edc
Muhammad Muddasir Hussain <m.muddasir623@maqboolsolutions.com>
2022-03-17 09:55:55 +0500

    Sample project for testing jpro responsive.

--- src/main/java/com/jpro/hellojpro/page/InfoPage.java
+++ src/main/java/com/jpro/hellojpro/page/InfoPage.java
@@ -1,32 +1,65 @@
 package com.jpro.hellojpro.page;
 
+import com.jpro.web.View;
 import com.jpro.webapi.WebAPI;
 import com.sandec.mdfx.MarkdownView;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
 
-public class InfoPage extends DefaultPage {
+public class InfoPage extends View {
 
     private File readmeFile = new File("README.md");
 
-    public InfoPage(WebAPI webAPI) {
-        super(webAPI);
+    @Override
+    public String title() {
+        return "Markdown Test Project";
     }
 
     @Override
-    public Node pageContent() {
+    public String description() {
+        return "";
+    }
+
+    @Override
+    public Node content() {
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(10));
+        borderPane.getStylesheets().add("/com/sandec/mdfx/mdfx-default.css");
+
+        VBox vbxMain = new VBox(10);
+
+        String readmeStr = "";
         try {
-            String readmeStr = IOUtils.toString(readmeFile.toURI()   , "utf-8");
-            MarkdownView mview = new MarkdownView(readmeStr);
-            StackPane infoContainer = new StackPane(mview);
-            infoContainer.getStyleClass().add("info-container");
-            return infoContainer;
+            readmeStr = IOUtils.toString(readmeFile.toURI()   , "utf-8");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        MarkdownView mview = new MarkdownView(readmeStr);
+
+        VBox hbxFooter = new VBox(10);
+        hbxFooter.setStyle("-fx-background-color: blue");
+        hbxFooter.setPadding(new Insets(10));
+
+        for (int i = 1; i <= 200; i++) {
+            Label lbl = new Label(i + " Hello JPro");
+            lbl.setFont(new Font(25));
+            lbl.setStyle("-fx-text-fill: white");
+
+            hbxFooter.getChildren().add(lbl);
+        }
+
+        vbxMain.getChildren().addAll(mview, hbxFooter);
+
+        borderPane.setCenter(vbxMain);
+
+        return borderPane;
     }
 }
